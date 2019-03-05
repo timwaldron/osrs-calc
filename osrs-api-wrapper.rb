14daaf64e7f456cd8eeb162bce9d6085cb23fd6e
Timothy Waldron <tim@waldron.im>
2019-03-05 13:13:20 +1100

    Implemented class to integrate with RuneScape hiscore API and sort data into hashes

--- osrs-api-wrapper.rb
+++ osrs-api-wrapper.rb
@@ -1,9 +1,11 @@
 require 'net/http'
+require 'csv'
 
 class OSRS_Api_Wrapper
     # Downloads, evaluates then returns API data (Grand Exchange/High-score)
 
     def initialize()
+        # @ign = nil
         @osrs_base_url = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player="
     end
 
@@ -11,10 +13,23 @@ class OSRS_Api_Wrapper
         print("Username: ")
         ign = gets().strip()
 
-        hiscore_data = Net::HTTP.get(URI(@osrs_base_url + ign)) # => String
-
+        raw_hiscore_order = ["overall", "attack", "defence", "strength", "hitpoints", "ranged", "prayer", "magic", "cooking", "woodcutting", "fletching", "fishing", "firemaking", "crafting", "smithing", "mining", "herblore", "agility", "thieving", "slayer", "farming", "runecrafting", "hunter", "construction", "bounty_hunter", "bounty_hunter_rogues", "clue_scrolls_all", "clue_scrolls_easy", "clue_scrolls_medium", "clue_scrolls_hard", "clue_scrolls_elite", "clue_scrolls_master", "lms"]
+        raw_hiscore_data = "rank,level,experience\n"
+        raw_hiscore_data += Net::HTTP.get(URI(@osrs_base_url + ign)) # => String
+        hashed_hiscore_data = CSV.parse(raw_hiscore_data, :headers => true)
+        hash_test = {}
         puts("========== HISCORE DATA RESPONSE ==========")
-        puts("#{hiscore_data}")
+
+        hashed_hiscore_data.each_with_index do |row, index|
+            row_data = row.to_hash
+
+            hash_test[raw_hiscore_order[index]] = row_data
+        end
+
+        hash_test.each do |key, value|
+            puts("#{key}:\t\t#{value}")
+        end
+
         puts("========== HISCORE DATA RESPONSE ==========")
     end
 end
