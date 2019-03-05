a71744c2f82942869b4c699b63b0a758b104a758
BradleyJrichardson <brdl.richardson@gmail.com>
2019-03-05 16:59:54 +1100

    adding data and improved logic

---
+++ calc_data.rb
@@ -0,0 +1,32 @@
+monsters_for_combat = {
+  "Cow": 32,
+  "Woman": 28,
+  "Guard": 88,
+  "Rock Crab": 200,
+  "Hill Giant": 330,
+  "Black Dragon": 800,
+}
+
+cooking = {
+  "Shrimps": 30,
+  "Trout": 70,
+  "Tuna": 100,
+  "Anchovy Pizza": 182,
+  "Manta ray": 216,
+}
+
+woodcutting = {
+  "Logs": 25,
+  "Oak logs": 40,
+  "Maple logs": 100,
+  "Yew logs": 175,
+  "Magic logs": 250,
+}
+
+hunter = {
+  "Crimson Swift": 34,
+  "Wild Kebbit": 128,
+  "Razor-backed Kebbit": 348,
+  "Dark Kebbit": 132,
+  "Red Salamander": 272,
+}
