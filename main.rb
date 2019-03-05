bb84a0a39db6806c8b6bccd5a9466f7b50df81e2
BradleyJrichardson <brdl.richardson@gmail.com>
2019-03-05 13:13:38 +1100

    starting work on interface of application

--- main.rb
+++ main.rb
@@ -1,6 +1,60 @@
-class OSRS_Calc
+class Main
+  def initialize()
+    @ign = nil
+    @choice = nil
 
-    def initialize()
+    app
+  end
 
+  def app
+    login
+    menu
+  end
+
+  def login
+    puts `clear`
+    puts "Welcome to Runescape"
+    puts "Please enter existing username"
+    ign = gets.strip.downcase
+  end
+
+  def menu
+    puts "--------- Old School RuneScape skill calculator ----------"
+    puts "----------------------- Options ------------------------"
+    puts "View your Skills - Press 1"
+    puts "Skill Calculator - Press 2"
+    puts "Enter a new username - Press 3"
+    # choice = gets.strip
+    loop_logic(gets.strip.to_i)
+  end
+
+  def loop_logic(choice)
+    case choice
+    when 1
+      display_skills
+      puts "enter to exit"
+      input = gets.strip
+      if ""
+        app
+      end
+    when 2
+      skill_calculator
+      puts "enter to exit"
+      input = gets.strip
+      if ""
+        app
+      end
+    when 3
+      puts "test 3"
+      app
     end
+  end
+
+  def display_skills
+  end
+
+  def skill_calculator
+  end
 end
+
+osrs_app = Main.new()
