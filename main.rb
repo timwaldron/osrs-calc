34994a35a805894e432459ddb858351f18909c8a
Timothy Waldron <tim@waldron.im>
2019-03-06 09:16:57 +1100

    Changed/added file names, prettified terminal

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-require "./osrs-api-wrapper"
-require "awesome_print"
-
+require './osrs-api-wrapper'
+require 'awesome_print'
+require 'tty-spinner'
 
 class Main
   def initialize()
@@ -13,12 +13,35 @@ class Main
 
   def login
     while true
+        pls_or_sorry_pls_string = "Please" # If they enter a username not on the OSRS hiscores
       while @player_data == false
         puts `clear`
-        puts "Welcome to Runescape"
-        puts "Please enter existing username"
+        puts "       Welcome to the" # Below looks mangled, but because it uses the escape sequence I had to replace every '\' with '\\'
+        puts " ____                   ____                         "     
+        puts "|  _ \\ _   _ _ __   ___/ ___|  ___ __ _ _ __   ___   "      
+        puts "| |_) | | | | '_ \\ / _ \\___ \\ / __/ _` | '_ \\ / _ \\  "      
+        puts "|  _ <| |_| | | | |  __/___) | (_| (_| | |_) |  __/  "      
+        puts "|_| \\_\\\\__,_|_| |_|\\___|____/ \\___\\__,_| .__/ \\___|  "     
+        puts "                                       |_|           "    
+        puts ""  
+        puts "   S  K  I  L  L      C  A  L  C  U  L  A  T  O  R"
+        puts ""
+        print "#{pls_or_sorry_pls_string} enter a valid username (or '!exit' to quit): "
+
         @username = gets.strip.downcase
-        @player_data = @osrs_api.username_exists?(@username)
+
+        if (@username == "!exit")
+            exit!
+        end
+
+        spinner = TTY::Spinner.new("[:spinner] Checking if #{@username} exists ...", format: :classic)
+        spinner.auto_spin # Automatic animation with default interval
+        @player_data = @osrs_api.get_hiscore_data(@username)
+        spinner.stop('Done!') # Stop animation
+
+        if (@player_data == false) # If it player data returns false it wasn't sucessful.
+            pls_or_sorry_pls_string = "Sorry,"
+        end
       end
       menu
     end
